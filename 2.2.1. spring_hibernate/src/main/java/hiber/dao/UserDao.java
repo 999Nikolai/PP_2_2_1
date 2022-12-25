fa225fc1bb4d9d53cb02c2e7537ce307d7eeed17
999Nikolai <90135496+999Nikolai@users.noreply.github.com>
2022-12-26 00:08:45 +0300

    2_2_1 PP 26/12/22

--- 2.2.1. spring_hibernate/src/main/java/hiber/dao/UserDao.java
+++ 2.2.1. spring_hibernate/src/main/java/hiber/dao/UserDao.java
@@ -1,10 +1,13 @@
 package hiber.dao;
 
+import hiber.model.Car;
 import hiber.model.User;
 
 import java.util.List;
 
 public interface UserDao {
    void add(User user);
+   User getUserCar (String mmodel, int sseries);
+
    List<User> listUsers();
 }
